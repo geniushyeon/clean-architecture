655ab93fe82576227c6bce5657c0efbabcb6e21c
geniushyeon <jhlee0912@icloud.com>
2023-07-13 20:16:12 +0900

    🎉 project init

---
+++ src/test/java/com/geniuishyeon/architecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geniuishyeon.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
